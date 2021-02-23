62a9d2e68c6e89a4d9fe75e8f9d7c5a055bb9a6d
Andrei Kazakov <andrey.kazakov@loudplay.io>
2021-02-23 23:36:38 +0300

    add geometry target to build all tests

--- geometry/tests/intersect.cpp
+++ geometry/tests/intersect.cpp
@@ -1,23 +1,17 @@
+#include <catch2/catch.hpp>
 #include <geometry/algorythms/intersect.hpp>
 #include <geometry/flat/line.hpp>
 
 using line2d = geometry::flat::line<double>;
 using point2d = geometry::flat::point<double>;
 
-/*
-BOOST_AUTO_TEST_SUITE(intersect2d)
+TEST_CASE("Function can calculate intersection of 2 2d points", "[intersect]")
+{
+	line2d l1({1, 1}, {6, 6});
+	line2d l2({5, 2}, {3, 6});
 
-	BOOST_AUTO_TEST_CASE(line_to_line)
-	{
-		line2d l1({1, 1}, {6, 6});
-		line2d l2({5, 2}, {3, 6});
+	point2d value = geometry::intersect(l1, l2);
 
-		point2d value = geometry::intersect(l1, l2);
-
-		BOOST_CHECK_CLOSE(value.x(), 4, 0.001);
-		BOOST_CHECK_CLOSE(value.y(), 4, 0.001);
-	}
-
-BOOST_AUTO_TEST_SUITE_END()
-*/
-int main() {}
+	REQUIRE_THAT(value.x(), Catch::Matchers::WithinRel(4, 0.0001));
+	REQUIRE_THAT(value.y(), Catch::Matchers::WithinRel(4, 0.0001));
+}
