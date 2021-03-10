880121186e69fc5d37190a10f0a85ef5d917f5d9
Andrei Kazakov <andrey.kazakov@loudplay.io>
2021-03-10 12:27:18 +0300

    add endpoint test

--- network/tests/endpoint.cpp
+++ network/tests/endpoint.cpp
@@ -4,72 +4,69 @@
 using network::ip::address;
 using network::ip::tcp;
 
-TEST_CASE("Default constructor", "[endpoint]")
+TEST_CASE("Endpoint can be defaul constructed", "[endpoint]")
 {
 	tcp::endpoint ep;
 
-	sockaddr* data = ep.data();
-
-	//REQUIRE(data->sa_family == AF_INET);
-
-	REQUIRE(ep.address().to_v4() == address::v4::any());
-	REQUIRE(ep.port() == 0);
-	REQUIRE(ep.size() == 16);
-	REQUIRE(ep.is_v4());
-	REQUIRE_FALSE(ep.is_v6());
+	CHECK(ep.address().to_v4() == address::v4::any());
+	CHECK_THROWS_AS(ep.address().to_v6(), std::bad_cast);
+	CHECK(ep.port() == 0);
+	CHECK(ep.size() == 16);
+	CHECK(ep.is_v4());
+	CHECK_FALSE(ep.is_v6());
 }
 
-TEST_CASE("Protocol constructor", "[endpoint]")
+TEST_CASE("Endpoint can be constructed via ipv6 protocol", "[endpoint]")
 {
-	tcp::endpoint ep(tcp::v6());
+	tcp::endpoint ep{tcp::v6()};
 
-	sockaddr* data = ep.data();
-
-	//REQUIRE(data->sa_family == AF_INET6);
-
-	REQUIRE(ep.address().to_v6() == address::v6::any());
-	REQUIRE(ep.port() == 0);
-	REQUIRE(ep.size() == 28);
-	REQUIRE_FALSE(ep.is_v4());
-	REQUIRE(ep.is_v6());
+	CHECK_THROWS_AS(ep.address().to_v4(), std::bad_cast);
+	CHECK(ep.address().to_v6() == address::v6::any());
+	CHECK(ep.port() == 0);
+	CHECK(ep.size() == 28);
+	CHECK_FALSE(ep.is_v4());
+	CHECK(ep.is_v6());
 }
 
-TEST_CASE("Protocol/port constructor", "[endpoint]")
+TEST_CASE("Endpoint can be constructed via ipv4 protocol", "[endpoint]")
 {
-	tcp::endpoint ep(tcp::v4(), 400);
+	tcp::endpoint ep{tcp::v4()};
 
-	sockaddr* data = ep.data();
-
-	//REQUIRE(data->sa_family == AF_INET);
-
-	REQUIRE(ep.address().to_v4() == address::v4::any());
-	REQUIRE(ep.port() == 400);
-	REQUIRE(ep.size() == 16);
-	REQUIRE(ep.is_v4());
-	REQUIRE_FALSE(ep.is_v6());
+	CHECK(ep.address().to_v4() == address::v4::any());
+	CHECK_THROWS_AS(ep.address().to_v6(), std::bad_cast);
+	CHECK(ep.port() == 0);
+	CHECK(ep.size() == 16);
+	CHECK(ep.is_v4());
+	CHECK_FALSE(ep.is_v6());
 }
 
-TEST_CASE("Address constructors", "[endpoint]")
+TEST_CASE("Endpoint can be constructed via protocol and port number", "[endpoint]")
 {
-	tcp::endpoint ep(address::v4::any(), 1000);
+	tcp::endpoint ep{tcp::v4(), 400};
+
+	CHECK(ep.address().to_v4() == address::v4::any());
+	CHECK_THROWS_AS(ep.address().to_v6(), std::bad_cast);
+	CHECK(ep.port() == 400);
+	CHECK(ep.size() == 16);
+	CHECK(ep.is_v4());
+	CHECK_FALSE(ep.is_v6());
+}
+
+TEST_CASE("Encpoint can be constructed via specific address", "[endpoint]")
+{
+	tcp::endpoint ep{address::v4::any(), 1000};
 
 	sockaddr* data = ep.data();
 
-	//REQUIRE(data->sa_family == AF_INET);
+	CHECK(ep.port() == 1000);
+	CHECK(ep.size() == 16);
+	CHECK(ep.is_v4());
+	CHECK_FALSE(ep.is_v6());
 
-	REQUIRE(ep.port() == 1000);
-	REQUIRE(ep.size() == 16);
-	REQUIRE(ep.is_v4());
-	REQUIRE_FALSE(ep.is_v6());
+	tcp::endpoint ep2{address::v6::loopback(), 555};
 
-	tcp::endpoint ep2(address::v6::loopback(), 555);
-
-	data = ep2.data();
-
-	//REQUIRE(data->sa_family == AF_INET6);
-
-	REQUIRE(ep2.port() == 555);
-	REQUIRE(ep2.size() == 28);
-	REQUIRE_FALSE(ep2.is_v4());
-	REQUIRE(ep2.is_v6());
+	CHECK(ep2.port() == 555);
+	CHECK(ep2.size() == 28);
+	CHECK_FALSE(ep2.is_v4());
+	CHECK(ep2.is_v6());
 }
